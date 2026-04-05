348cebdc3dbb581843fe5f145aad5e573adb4f21
Sehwag LK <veersehwaglk@gmail.com>
2026-04-06 00:40:27 +0530

    final fix Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,10 @@ import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
-// ✅ Proper imports
 import Techstack from "../About/Techstack";
 import Github from "../About/Github";
 
-import {
-  AiFillGithub,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
@@ -23,16 +19,10 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+              <h1 className="heading">Hi There! 👋🏻</h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SEHWAG LK</strong>
+                I'M <strong className="main-name">SEHWAG LK</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -40,66 +30,41 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+            <Col md={5}>
               <img
                 src={homeLogo}
-                alt="home pic"
+                alt="home"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
               />
             </Col>
           </Row>
         </Container>
       </Container>
 
-      {/* About Section */}
       <Home2 />
-
-      {/* 🔥 Tech Stack */}
       <Techstack />
-
-      {/* 🔥 GitHub / LeetCode Section */}
       <Github />
 
-      {/* Social Links */}
       <Container>
-        <Row style={{ paddingTop: "50px", paddingBottom: "80px" }}>
+        <Row style={{ padding: "50px 0" }}>
           <Col md={12} className="home-about-social">
             <h1>Find Me On</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
 
             <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/SehwagLK"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
+              <li>
+                <a href="https://github.com/SehwagLK" target="_blank" rel="noreferrer">
                   <AiFillGithub />
                 </a>
               </li>
 
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/veer-sehwag-lk-1842072b8/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
+              <li>
+                <a href="https://linkedin.com/in/veer-sehwag-lk-1842072b8/" target="_blank" rel="noreferrer">
                   <FaLinkedinIn />
                 </a>
               </li>
 
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/sehwag_lk"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
+              <li>
+                <a href="https://instagram.com/sehwag_lk" target="_blank" rel="noreferrer">
                   <AiFillInstagram />
                 </a>
               </li>
